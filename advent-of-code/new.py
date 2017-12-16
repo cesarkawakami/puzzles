5d7fd0a368aff3de27abba82c383a84a7e6dfc17
Cesar Kawakami <me@crkw.io>
2017-12-15 16:12:24 -0800

    script for new days

---
+++ advent-of-code/new.py
@@ -0,0 +1,49 @@
+#!/home/cesarkawakami/pcode/puzzles/advent-of-code/venv/bin/python3
+
+import os
+import os.path
+import requests
+from argparse import ArgumentParser
+from typing import Dict
+
+
+def get_cookies() -> Dict[str, str]:
+    rv = {}
+    with open(os.path.join(os.environ['HOME'], '.adventofcode'), 'r') as f:
+        for line in f:
+            cookie_name, cookie_value = line.rstrip('\n').split('=')
+            rv[cookie_name] = cookie_value
+    return rv
+
+
+def download_input(year: int, day: int) -> str:
+    cookies = get_cookies()
+    response = requests.get(
+        f'http://adventofcode.com/{year}/day/{day}/input',
+        cookies=cookies,
+    )
+    return response.text
+
+
+def main() -> None:
+    parser = ArgumentParser()
+    parser.add_argument('year', type=int)
+    parser.add_argument('day', type=int)
+    args = parser.parse_args()
+
+    input_string = download_input(args.year, args.day)
+
+    day_path = os.path.join(
+        os.path.dirname(__file__),
+        str(args.year),
+        f'day{args.day}',
+    )
+    for part in ['part1', 'part2']:
+        part_path = os.path.join(day_path, part)
+        os.makedirs(part_path, exist_ok=True)
+        with open(os.path.join(part_path, 'input'), 'w') as f:
+            f.write(input_string)
+
+
+if __name__ == '__main__':
+    main()
